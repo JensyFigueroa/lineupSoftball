0788d85ec6ef548c80bc7fd501e015a305609663
Jensy Figueroa <jensy1418@gmail.com>
2024-02-07 19:00:39 -0500

    Se corrige la forma de mostrar las imagenes

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import styles from './Home.module.css'
+import photoChampions from '../../assets/fotos/Champions.png'
 const Home = () => {
   return (
     <div className={styles.home}>
         <h1>We are the champions</h1>
-      <img src="/src/assets/fotos/Champions.png" alt=""  />
+      <img src={photoChampions} alt=""  />
     </div>
   )
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,12 @@
 import { Link, NavLink } from 'react-router-dom'
 import styles from './Navbar.module.css'
-import img from '../../assets/Logo.png'
+import logo from '../../assets/Logo.png'
 
 export default function Navbar() {
   return (
     <div className={styles.navBarGrid}>
       
-        <Link to='/'><img src={img} alt=""  className={styles.img}/></Link> 
+        <Link to='/'><img src={logo} alt=""  className={styles.img}/></Link> 
 
       <div className={styles.gridItemMenu}>
         <NavLink to='/home' className={({ isActive }) => (isActive ? 'active' : 'link')}>Home</NavLink>
