f4010aa539abac3b829c80b8bc454dbbd267480d
Jensy Figueroa <jensy1418@gmail.com>
2024-03-22 12:09:39 -0400

    Fixed landing page styles

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -4,17 +4,13 @@ import logo from '../../assets/Logo.png'
 
 export default function LandingPage() {
   return (
-    <div className={styles.boxLanding}>
       <div className={styles.main}>
-
         <img src={logo} alt="" />
         <h1>Welcome to the official page of Bebedores</h1>
-        {/* <h4>Where you can create your Cards with your favorite video game</h4> */}
         <Link to="/home" className={styles.btn}>
           Visit now
         </Link>
       </div>
-      
-    </div>
+  
   );
 }
